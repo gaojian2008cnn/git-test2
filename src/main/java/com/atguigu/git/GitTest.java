b6a7cd5a935ca93a509680f95e8ea13ee174caa6
gaojian <gaojian2008cnn@126.com>
2023-01-06 16:05:12 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * 类 GitTest .
+ *
+ * @version v1.0.0
+ * @author:gaojian
+ * @createTime：2023/1/6 16:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("gitTest");
+    }
+}
