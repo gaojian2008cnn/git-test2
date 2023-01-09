c23528a114af60e360d6c7d11f237ae9f9d6c8f6
gaojian <gaojian2008cnn@126.com>
2023-01-09 11:23:49 +0800

    master第二次提交

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("核力量");
         System.out.println("核力量");
         System.out.println("hot-fix");
+        System.out.println("master二");
     }
 }
